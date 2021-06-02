d043725361903f453f47256963337d08a27bc106
An <53161441+anbinhpham1107@users.noreply.github.com>
2021-06-02 16:45:43 -0500

    Updates

---
+++ precache-manifest.c5ed2020ba414e52466409220dd48c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0358564c1b9204102937d7ce7bc68fd",
+    "url": "/memo-game-deploy/index.html"
+  },
+  {
+    "revision": "e4c67349a425455f6c5b",
+    "url": "/memo-game-deploy/static/css/main.f177fd0f.chunk.css"
+  },
+  {
+    "revision": "1994df78ad894add1fd3",
+    "url": "/memo-game-deploy/static/js/2.d1108a1a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memo-game-deploy/static/js/2.d1108a1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4c67349a425455f6c5b",
+    "url": "/memo-game-deploy/static/js/main.76a5ebcb.chunk.js"
+  },
+  {
+    "revision": "fde74d8e41e08e1f97d7",
+    "url": "/memo-game-deploy/static/js/runtime-main.8ece8e9c.js"
+  }
+]);
